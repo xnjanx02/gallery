7b59800459ff7813436977df2f09ccae9b673f45
xnjanx02 <anjana.jk009@gmail.com>
2022-07-08 13:07:02 +0530

    New component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewgalleryComponent } from './viewgallery/viewgallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewgalleryComponent
   ],
   imports: [
     BrowserModule,
